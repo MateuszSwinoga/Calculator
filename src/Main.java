e8d2104e18635e0137329df4ad7bbf4bffc09d72
Mateusz Świnoga <mateusz19111@gmail.com>
2022-09-21 18:03:49 +0200

    Added int and case1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("What do you want to do?\n");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first value");
+        int x = scanner.nextInt();
+        System.out.println("Enter second value");
+        int y = scanner.nextInt();
+        System.out.println("What do you want to do?\n1.addition\n2.substraction\n3.multiplication\n4.division");
+        int select = scanner.nextInt();
+
+        switch (select) {
+            case 1:
+                System.out.println();
+        }
 
     }
 }
