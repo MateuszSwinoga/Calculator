7e950468df8a86a2f0a336b0bbf4be6c61b96c07
Mateusz Świnoga <mateusz19111@gmail.com>
2022-09-24 21:38:26 +0200

    added scaner x, y and reutrn msg

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        Scanner variables = new Scanner(System.in);
+        System.out.println("Set x");
+        float x = variables.nextFloat();
+        System.out.println("Set y");
+        float y = variables.nextFloat();
+
+
+
         Calculator cal1 = new Calculator();
-        System.out.println(cal1.addition(5,8));
-        System.out.println(cal1.substraction(5,8));
-        System.out.println((cal1.multiplication(5,8)));
-        System.out.println(cal1.division(5,8));
+        System.out.println("Addition resoult: " + cal1.addition(x,y));
+        System.out.println("Substraction resoult: " + cal1.substraction( x,y));
+        System.out.println("Multiplication resoult: " + cal1.multiplication( + x,y));
+        System.out.println("Division Resoult: " + cal1.division(+ x,y));
     }
 }
